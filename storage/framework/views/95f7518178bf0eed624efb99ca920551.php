ae15c4c926c0ff2c16ed0f08a8f047abec150a35
afriklabprojet <afriklabprojet@gmail.com>
2026-02-19 18:44:24 +0100

    Fix: gitignore sessions, vues compilées, coverage

--- storage/framework/views/95f7518178bf0eed624efb99ca920551.php
+++
@@ -1,3 +0,0 @@
-<?php echo e($slot); ?>
-
-<?php /**PATH /Users/teya2023/Downloads/invoice-saas-starter/vendor/laravel/framework/src/Illuminate/Mail/resources/views/text/table.blade.php ENDPATH**/ ?>
